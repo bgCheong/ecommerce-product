6b4d0a55fd37b091f1d17f53bebd082555c95416
jbg <jbg4354@naver.com>
2025-07-20 12:42:52 +0900

    update

--- src/main/java/com/example/productservice/entity/Product.java
+++ src/main/java/com/example/productservice/entity/Product.java
@@ -1,15 +1,22 @@
 package com.example.productservice.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "products")
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,4 +26,10 @@ public class Product {
     private Integer price;
     private String description;
     private Integer stock;
+    @Column(name = "start_date")
+    private String startDate;
+    @Column(name = "end_date")
+    private String endDate;
+    private String status = "Y";
+    
 }
